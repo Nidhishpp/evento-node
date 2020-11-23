843a2ca37ab3bcf7839bb01e965044e373ee441b
Nidhish PP <nidhishpp.pp@gmail.com>
2020-11-24 02:05:38 +0530

    dp update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,6 +2,6 @@ module.exports = {
     HOST: "starter.xjmjy.mongodb.net",
     PORT: 27017,
     DB: "evento_db",
-    USERNAME: "nidhish",
-    PASSWORD: "Nidhi654!",
+    USERNAME: "evento_user",
+    PASSWORD: "myeventoapp",
 };
